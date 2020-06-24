1c42528812f983893433f54a7f05fbf279d8f935
[derian-rods] <[derian-rods@gmail.com]>
2020-06-24 22:37:35 +0200

    [fix] responsive, complet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-import tanya from './assets/img/image-tanya.jpg'
-import john from './assets/img/image-john.jpg'
 let developers = [{
     name: 'Tanya Sinclair',
     role: 'UX Engineer',
-    avatar: tanya,
+    avatar: 'https://raw.githubusercontent.com/derian-rods/Coding-bootcamp-testimonials-slider/master/src/assets/img/image-tanya.jpg',
     text: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
   },
   {
     name: ' John Tarkpor',
     role: 'Junior Front-end Developer',
-    avatar: john,
+    avatar: 'https://raw.githubusercontent.com/derian-rods/Coding-bootcamp-testimonials-slider/master/src/assets/img/image-john.jpg',
     text: '“ If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
   },
 ];
